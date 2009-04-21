bddb8254a750811ed3dd3ba5f0cc7be08d129e61
Andrew Porter <partydrone@me.com>
2009-04-21 14:19:27 -0600

    Trying to work out a custom link_to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def link_selected_to(str, regexp, path, attributes = {})
+    # if the current base url is the same as the controller, then set :class = 'selected'
+    if params[:controller] =~ regexp
+      attributes['class'] = 'selected'
+    end
+    
+    # construct the li and a tags
+    link_to "<span>#{str}</span>", path, attributes
+  end
+  
 end

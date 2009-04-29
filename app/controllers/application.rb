d8c50e2a7599e72cf05f60bc6fced399d25def6a
Andrew Porter <partydrone@me.com>
2009-04-29 16:13:37 -0600

    Reimplemented restful_authentication in default state

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,7 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  # Include AuthenticationSystem
+  # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
   
   helper :all # include all helpers, all the time

3d97bd793fe1d0f89cbc788b6516e697037f0bc6
Andrew Porter <partydrone@me.com>
2009-05-01 14:39:06 -0600

    Added user index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   
   before_filter { |c| c.send :set_page_title, ['Products'] }
+  
   caches_page :index
   
   def index
--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
   
   test "invalid with empty attributes" do
     article = Article.new
@@ -13,4 +9,5 @@ class ArticleTest < ActiveSupport::TestCase
     assert article.errors.invalid?(:body)
     assert article.errors.invalid?(:post_date), "saving #{:post_date}"
   end
+  
 end

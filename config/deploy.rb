49ee9e37fe540df8fa84ea0144e4ca789ffad6ae
Andrew Porter <partydrone@me.com>
2009-05-03 23:31:18 -0600

    Updated depoloy configuration for Media Temple

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,53 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'rt_capistrano'
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :site,         "8898"
+set :application,  "summit-traffic"
+set :webpath,      "summit-traffic.com"
+set :domain,       "eyequeue.us"
+set :user,         "serveradmin%eyequeue.us"
+set :password,     "dogVE13@"
+set :use_sudo,      false
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+# repository elsewhere
+set :scm, :git
+set :repository, "git@home.eyequeue.us:Repositories/summit-traffic.git"
+set :ssh_options, {:forward_agent => true}
+set :branch, "master"
+set :deploy_via, :remote_cache
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+# these shouldn't be changed
+role :web, "#{domain}"
+role :app, "#{domain}"
+role :db,  "#{domain}", :primary => true
+set :deploy_to,    "/home/#{site}/containers/rails/#{application}"
+
+namespace :deploy do
+  desc "Creates the database configuration file in the application's config directory."
+  task :update_config, :roles => :app do
+    database_yml = <<-CMD
+      production:
+        adapter: mysql
+        database: db8898_summit-traffic_production
+        username: db8898
+        password: dogVE13@db
+        host: internal-db.s8898.gridserver.com
+    CMD
+    put database_yml, "#{release_path}/config/database.yml"
+  end
+  after "deploy:update_code", "deploy:update_config"
+  
+  namespace :web do
+    desc "Serve up a custom maintenance page."
+    task :disable, :roles => :web do
+      require 'erb'
+      on_rollback { run "rm #{shared_path}/system/maintenance.html" }
+      
+      reason = ENV['REASON']
+      deadline = ENV['UNTIL']
+      template = File.read("app/views/admin/maintenance.html.erb")
+      page = ERB.new(template).result(binding)
+      
+      put page, "#{shared_path}/system/maintenance.html", :mode => 0644
+    end
+  end
+end

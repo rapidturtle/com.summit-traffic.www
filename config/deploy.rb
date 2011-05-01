b48b4d381f1a190a97308ef2432cba0a24635512
Andrew Porter <aporter@rapidturtle.com>
2011-05-01 00:24:59 -0600

    If not deploying code to the database server, it doesn't need to be in the recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_via, :remote_cache
 # servers
 role :web, "ve.eyequeue.us"
 role :app, "ve.eyequeue.us"
-role :db,  "external-db.s8898.gridserver.com", :primary => true
+# role :db,  "external-db.s8898.gridserver.com", :primary => true
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do

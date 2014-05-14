16776bb19bcef1158c22c670b1367c1ddda13e0d
Andrew Porter <aporter@rapidturtle.com>
2014-05-14 00:25:12 -0600

    Minor page title tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def full_title(page_title)
     base_title = "Summit Traffic Solutions"
     if page_title.present?
-      "#{base_title} - #{page_title}"
+      "#{base_title} | #{page_title}"
     else
       base_title
     end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -4,7 +4,7 @@ class ApplicationHelperTest < ActionView::TestCase
   # full_title
 
   test "should format a full title when an argument is given" do
-    assert_equal "Summit Traffic Solutions - Contact", full_title("Contact")
+    assert_equal "Summit Traffic Solutions | Contact", full_title("Contact")
   end
 
   test "should return a base title when no argument is given" do

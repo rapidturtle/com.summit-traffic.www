8f388a0461e2011dacc25306ee8e0368be92d99f
Andrew Porter <aporter@rapidturtle.com>
2014-09-21 15:30:25 -0600

    Reset to empty Rails 4.2 project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def full_title(page_title)
-    base_title = "Summit Traffic Solutions"
-    if page_title.present?
-      "#{base_title} | #{page_title}"
-    else
-      base_title
-    end
-  end
 end

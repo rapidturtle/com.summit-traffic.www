683cba3dba2316ad66f2bcb438f73ed2f87e0221
Andrew Porter <aporter@rapidturtle.com>
2015-02-06 23:26:41 -0700

    Update Capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.1
-lock '3.2.1'
+lock '3.3.5'
 
 set :application, 'com.summit-traffic.www'
 set :repo_url, "git@github.com:rapidturtle/#{fetch(:application)}.git"
